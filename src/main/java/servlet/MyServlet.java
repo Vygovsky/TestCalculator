766f98b68fe36f726db8489d6e4d3873e7e5d638
Roman_v <vigmol@ukr.net>
2018-07-27 17:27:25 +0300

    new page

--- src/main/java/servlet/MyServlet.java
+++ src/main/java/servlet/MyServlet.java
@@ -8,9 +8,50 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/hello")
+@WebServlet("/start")
 public class MyServlet extends HttpServlet {
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      /*  resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");*/
+//or
+        resp.setContentType("text/html; charset=utf-8");
+
+        String name = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        System.out.println(name);
+        System.out.println(lastName);
+
+
+        PrintWriter writer = resp.getWriter();
+        //writer.print("<h1>Hello Роман и " + name + " " + lastName + " doGet</h1> <br> Agane");
+        writer.print("<h3>Профиль сотрудника</h3>");
+        writer.print("Имя: " + name + "<br>");
+        writer.print("Фимилия : " + name + "<br>");
+
+        writer.close();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      /*  resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");*/
+//or
+        req.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=utf-8");
+
+        String name = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        System.out.println(name);
+        System.out.println(lastName);
+
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>Hello Роман и " + name + " " + lastName + " doPost</h1> <br> Agane");
+        writer.close();
+    }
+
+
+/* @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter pw=resp.getWriter();
         pw.print("Hello world mather fack");
@@ -21,5 +62,5 @@ public class MyServlet extends HttpServlet {
 
 
     }
-
+*/
 }
